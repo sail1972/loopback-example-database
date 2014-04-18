c4157923ee27d97abed8aebc5004c58dd6596856
Raymond Feng <raymond@strongloop.com>
2014-04-18 10:23:13 -0700

    Add postgreqsql config

--- discover.js
+++ discover.js
@@ -1,11 +1,11 @@
 var app = require('./app');
-var dataSource = app.dataSources.mysql;
+var dataSource = app.dataSources.postgresql;
 
 dataSource.discoverSchema('account', {owner: 'demo'}, function (err, schema) {
     console.log(JSON.stringify(schema, null, '  '));
 });
 
-dataSource.discoverAndBuildModels('account', {owner: 'demo'}, function (err, models) {
+dataSource.discoverAndBuildModels('account', {owner: 'public'}, function (err, models) {
     models.Account.find(function (err, act) {
         if (err) {
             console.error(err);
