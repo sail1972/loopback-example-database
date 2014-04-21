e826ae501016e39502b0e2cbb11c1f7ed2447b22
Raymond Feng <raymond@strongloop.com>
2014-04-21 09:03:16 -0700

    Fix the schema name

--- discover.js
+++ discover.js
@@ -5,7 +5,7 @@ dataSource.discoverSchema('account', {owner: 'demo'}, function (err, schema) {
     console.log(JSON.stringify(schema, null, '  '));
 });
 
-dataSource.discoverAndBuildModels('account', {owner: 'public'}, function (err, models) {
+dataSource.discoverAndBuildModels('account', {owner: 'demo'}, function (err, models) {
     models.Account.find(function (err, act) {
         if (err) {
             console.error(err);
